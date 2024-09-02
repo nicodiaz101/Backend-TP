059667363b24f34bedb95a004c445348a80c40f6
TomyRojasPonce <111310431+TomyRojasPonce@users.noreply.github.com>
2024-09-02 18:52:41 -0300

    saque CategoryRequest

--- src/main/java/com/uade/tpo/demo/entity/dto/OrderUserRequest.java
+++ src/main/java/com/uade/tpo/demo/entity/dto/OrderUserRequest.java
@@ -1,17 +1,9 @@
 package com.uade.tpo.demo.entity.dto;
 
-import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-public class CategoryRequest {
-    private int id;
-    private String description;
-}
-
 public class OrdenesComprasRequest {
-    @NotNull
-    @Size(min = 1, max = 100)
     private String user;
 
     private int cantidad;
